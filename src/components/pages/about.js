bd4546ef689ffd4c7ead1f7d7301664d18c4c4b3
Amrakhova Maryna <seagull2305@gmail.com>
2023-07-27 10:50:46 +0300

    new commit

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -4,7 +4,7 @@ const About = () => {
     return (        
         <div className='main-title'>            
             <h2> My background</h2>
-            <p>I`ve gone through several stages in my career, and each of them influenced my current work ethic and philosophy.
+            <p className="text">I`ve gone through several stages in my career, and each of them influenced my current work ethic and philosophy.
 I have excelled at communicating with clients, solving problems, and finding successful solutions for clients, and I believe that my main strength is my ability to adapt to any challenge that is thrown my way.
 </p>
         </div>           
